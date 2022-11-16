a931724e0269ac053fcd0a870e8d28cd0a59399a
Вадим Боровиков <vadim.borovikov@gmail.com>
2022-11-16 14:36:24 +0200

    DateTimeOffsetHelper helper added

---
+++ GryphonUtilities/DateTimeHelper.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+
+namespace GryphonUtilities;
+
+[PublicAPI]
+public static class DateTimeOffsetHelper
+{
+    public static DateOnly DateOnly(this DateTimeOffset dateTimeOffset)
+    {
+        return System.DateOnly.FromDateTime(dateTimeOffset.Date);
+    }
+
+    public static TimeOnly TimeOnly(this DateTimeOffset dateTimeOffset)
+    {
+        return System.TimeOnly.FromTimeSpan(dateTimeOffset.TimeOfDay);
+    }
+}
