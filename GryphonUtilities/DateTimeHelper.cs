e3febb7e46ba06601feb65d8f548aa84e1ab1d58
Вадим Боровиков <vadim.borovikov@gmail.com>
2022-11-16 22:33:52 +0200

    Passing TimeZoneInfo to FromOnly

--- GryphonUtilities/DateTimeHelper.cs
+++ GryphonUtilities/DateTimeHelper.cs
@@ -15,8 +15,10 @@ public static class DateTimeOffsetHelper
         return System.TimeOnly.FromTimeSpan(dateTimeOffset.TimeOfDay);
     }
 
-    public static DateTimeOffset FromOnly(DateOnly dateOnly, TimeOnly? timeOnly = null)
+    public static DateTimeOffset FromOnly(DateOnly dateOnly, TimeOnly? timeOnly = null,
+        TimeZoneInfo? timeZoneInfo = null)
     {
-        return new DateTimeOffset(dateOnly.ToDateTime(timeOnly ?? System.TimeOnly.MinValue));
+        return new DateTimeOffset(dateOnly.ToDateTime(timeOnly ?? System.TimeOnly.MinValue),
+            timeZoneInfo?.BaseUtcOffset ?? TimeSpan.Zero);
     }
 }
