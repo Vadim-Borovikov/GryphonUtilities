df4960522ae77682efc3bd0bf532e72f255ce7df
Вадим Боровиков <vadim.borovikov@gmail.com>
2022-12-03 04:23:51 +0200

    * EnumerableHelper added * Title added

---
+++ GryphonUtilities/EnumerableHelper.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace GryphonUtilities;
+
+[PublicAPI]
+public static class EnumerableHelper
+{
+    public static IEnumerable<T> Yield<T>(this T item)
+    {
+        yield return item;
+    }
+}
