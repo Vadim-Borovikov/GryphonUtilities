226d9ebcd5ca5e41e4d8ad090e448115b86d5160
Вадим Боровиков <vadim.borovikov@gmail.com>
2022-12-03 04:50:17 +0200

    WrapWithList added

--- GryphonUtilities/EnumerableHelper.cs
+++ GryphonUtilities/EnumerableHelper.cs
@@ -5,6 +5,8 @@ namespace GryphonUtilities;
 [PublicAPI]
 public static class EnumerableHelper
 {
+    public static List<T> WrapWithList<T>(this T item) => new() { item };
+
     public static IEnumerable<T> Yield<T>(this T item)
     {
         yield return item;
