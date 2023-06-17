f0e872ad822ea17dc17f349a1c362134a6956d0e
Вадим Боровиков <vadim.borovikov@gmail.com>
2023-06-17 11:50:43 +0200

    Russian numeric noun forms supported and tested

---
+++ GryphonUtilities/Text.cs
@@ -0,0 +1,47 @@
+﻿using JetBrains.Annotations;
+
+namespace GryphonUtilities;
+
+[PublicAPI]
+public static class Text
+{
+    public static string AddWord(string? first, string? second)
+    {
+        if (string.IsNullOrWhiteSpace(first))
+        {
+            return string.IsNullOrWhiteSpace(second) ? "" : second;
+        }
+
+        return string.IsNullOrWhiteSpace(second) ? first : $"{first} {second}";
+    }
+
+    public static string FormatLines(IEnumerable<string?> lines, params object[] args)
+    {
+        string format = JoinLines(lines);
+        return string.Format(format, args);
+    }
+
+    public static string FormatNumericWithNoun(string format, uint number, string form1, string form24,
+        string formAlot)
+    {
+        string form = GetNounForm(number, form1, form24, formAlot);
+        return string.Format(format, number, form);
+    }
+
+    public static string GetNounForm(uint number, string form1, string form24, string formAlot)
+    {
+        if (number is >= 11 and <= 14)
+        {
+            return formAlot;
+        }
+
+        return (number % 10) switch
+        {
+            1             => form1,
+            >= 2 and <= 4 => form24,
+            _             => formAlot
+        };
+    }
+
+    public static string JoinLines(IEnumerable<string?> lines) => string.Join(Environment.NewLine, lines);
+}
---
+++ GryphonUtilitiesTests/TextTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GryphonUtilities.Tests;
+
+[TestClass]
+public class TextTests
+{
+    [TestMethod]
+    public void GetNounFormTest()
+    {
+        AssertForm(1, Form1);
+        AssertForm(2, Form24);
+        AssertForm(3, Form24);
+        AssertForm(4, Form24);
+        for (uint i = 5; i <= 20; i++)
+        {
+            AssertForm(i, FormAlot);
+        }
+
+        for (uint i = 21; i <= 30; i++)
+        {
+            uint j = i - 20;
+            AssertForms(i, j);
+        }
+
+        for (uint i = 31; i <= 100; i++)
+        {
+            uint j = i - 10;
+            AssertForms(i, j);
+        }
+    }
+
+    [TestMethod]
+    public void FormatNumericWithNounTest()
+    {
+        AssertFormat("1 день", 1);
+        AssertFormat("7 дней", 7);
+        AssertFormat("32 дня", 32);
+        AssertFormat("365 дней", 365);
+    }
+
+    private static void AssertFormat(string expected, uint number)
+    {
+        Assert.AreEqual(expected, Text.FormatNumericWithNoun(Format, number, Form1, Form24, FormAlot));
+    }
+
+    private static void AssertForms(uint number1, uint number2)
+    {
+        Assert.AreEqual(Text.GetNounForm(number1, Form1, Form24, FormAlot),
+            Text.GetNounForm(number2, Form1, Form24, FormAlot));
+    }
+
+    private static void AssertForm(uint number, string expected)
+    {
+        Assert.AreEqual(expected, Text.GetNounForm(number, Form1, Form24, FormAlot));
+    }
+
+    private const string Form1 = "день";
+    private const string Form24 = "дня";
+    private const string FormAlot = "дней";
+    private const string Format = "{0} {1}";
+}
