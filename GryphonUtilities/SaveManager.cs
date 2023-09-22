5779526f80d4e5502a0d5344a6ab40c8e830504f
Вадим Боровиков <vadim.borovikov@gmail.com>
2023-09-22 12:39:50 +0300

    AfterLoad and BeforeSave Actions added

--- GryphonUtilities/SaveManager.cs
+++ GryphonUtilities/SaveManager.cs
@@ -9,10 +9,17 @@ public class SaveManager<T> : SaveManagerBase
 {
     public T Data { get; private set; }
 
-    public SaveManager(string path, TimeManager? timeManager = null) : base(path, timeManager) => Data = new T();
+    public SaveManager(string path, TimeManager? timeManager = null, Action? afterLoad = null,
+        Action? beforeSave = null) : base(path, timeManager)
+    {
+        _afterLoad = afterLoad;
+        _beforeSave = beforeSave;
+        Data = new T();
+    }
 
     public void Save()
     {
+        _beforeSave?.Invoke();
         lock (Locker)
         {
             string json = JsonSerializer.Serialize(Data, Options);
@@ -31,5 +38,9 @@ public class SaveManager<T> : SaveManagerBase
             string json = File.ReadAllText(Path);
             Data = JsonSerializer.Deserialize<T>(json, Options) ?? new T();
         }
+        _afterLoad?.Invoke();
     }
+
+    private readonly Action? _afterLoad;
+    private readonly Action? _beforeSave;
 }
